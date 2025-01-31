352fbf228acaba546e9a3aff5e9d27a17ac3db55
GARGEYAMITHRA <gargeyamithra18gali@gmail.com>
2025-01-31 15:26:07 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Routes,Route} from 'react-router-dom'
+import { LoginPage } from './Routes/Routes'
 function App() {
   
 
   return (
    <>
    <Routes>
-    <route path='/login' element={<LoginPage/>}/>
+    <Route path='/login' element={<LoginPage/>}/>
    </Routes>
    </>
   )
